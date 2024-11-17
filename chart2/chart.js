02877849b9115f623e62e7170701dda46fb92057
Wenielrana07 <rana.weniel@gmail.com>
2024-11-17 11:24:33 +0800

    add method fetch

--- chart2/chart.js
+++ chart2/chart.js
@@ -4,4 +4,19 @@ class Chart{
         this.barCtx = document.getElementById("barChart");
         this.pieCtx = document.getElementById("pieChart");
       }
+
+      async fetchData() {
+        try {
+          const response = await fetch(this.dataUrl);
+          if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+          }
+          return await response.json();
+        } catch (error) {
+          console.error(
+            "There has been a problem with your fetch operation:",
+            error
+          );
+        }
+      }
 }
