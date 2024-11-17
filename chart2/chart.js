09df7b4d909c35c9403c4c0c84adb4fd2bf05707
Wenielrana07 <rana.weniel@gmail.com>
2024-11-17 11:23:33 +0800

    add constructor

---
+++ chart2/chart.js
@@ -0,0 +1,7 @@
+class Chart{
+    constructor(dataUrl) {
+        this.dataUrl = dataUrl;
+        this.barCtx = document.getElementById("barChart");
+        this.pieCtx = document.getElementById("pieChart");
+      }
+}
